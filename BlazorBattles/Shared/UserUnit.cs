6804af6342f9a6ed0179c1930c8eb8d7e2a52eae
Patrick <patrick@godknows.de>
2020-09-21 06:54:19 +0200

    New Models for Knights & More

---
+++ BlazorBattles/Shared/UserUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBattles.Shared
+{
+    public class UserUnit
+    {
+        public int UserId { get; set; }
+        public int UnitId { get; set; }
+        public int HitPoints { get; set; }
+    }
+}
