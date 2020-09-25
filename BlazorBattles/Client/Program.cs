e93de0f4d597ecf5d40e421c242813bed345a480
Patrick <patrick@godknows.de>
2020-09-25 06:20:30 +0200

    Toaster Messages with Blazored.Toast

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using BlazorBattles.Client.Services;
+using Blazored.Toast;
 
 namespace BlazorBattles.Client
 {
@@ -18,6 +19,7 @@ namespace BlazorBattles.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Services.AddBlazoredToast();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
