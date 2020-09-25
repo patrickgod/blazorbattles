446b6d3bf779a77f7561cbae11cb545a36f1ecbb
Patrick <patrick@godknows.de>
2020-09-25 05:59:56 +0200

    Show Me Your Army

--- BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -20,9 +20,6 @@ namespace BlazorBattles.Client.Services
         {
             Unit unit = Units.First(unit => unit.Id == unitId);
             MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
-
-            Console.WriteLine($"{unit.Title} was built!");
-            Console.WriteLine($"Your army size: {MyUnits.Count}");
         }
     }
 }
