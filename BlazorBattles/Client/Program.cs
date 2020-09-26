7ef58ff5654793680eb7ea64bf29b55a8156507a
Patrick <patrick@godknows.de>
2020-09-26 13:48:01 +0200

    Expose the AuthenticationState & Introduce the AuthorizeView with Friends

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -24,6 +24,8 @@ namespace BlazorBattles.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IBananaService, BananaService>();
             builder.Services.AddScoped<IUnitService, UnitService>();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
             await builder.Build().RunAsync();
