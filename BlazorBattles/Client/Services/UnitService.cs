5567cdbbecc74f11bf1de78cf01881b1eac0fa15
Patrick <patrick@godknows.de>
2020-09-29 19:14:07 +0200

    Show Me Your Army

--- BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -48,5 +48,10 @@ namespace BlazorBattles.Client.Services
                 Units = await _http.GetFromJsonAsync<IList<Unit>>("api/unit");
             }
         }
+
+        public async Task LoadUserUnitsAsync()
+        {
+            MyUnits = await _http.GetFromJsonAsync<IList<UserUnit>>("api/UserUnit");
+        }
     }
 }
