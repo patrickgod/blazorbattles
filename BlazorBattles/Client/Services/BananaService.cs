6b40fd3e5dc03486194b9ba4484fe2340679fcc4
Patrick <patrick@godknows.de>
2020-09-29 08:10:02 +0200

    Receive the User’s Bananas on the Client

--- BlazorBattles/Client/Services/BananaService.cs
+++ BlazorBattles/Client/Services/BananaService.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace BlazorBattles.Client.Services
 {
     public class BananaService : IBananaService
     {
+        private readonly HttpClient _http;
+
+        public BananaService(HttpClient http)
+        {
+            _http = http;
+        }
+
         public event Action OnChange;
-        public int Bananas { get; set; } = 1000;
+        public int Bananas { get; set; } = 0;
 
         public void EatBananas(int amount)
         {
@@ -23,5 +32,11 @@ namespace BlazorBattles.Client.Services
         }
 
         void BananasChanged() => OnChange.Invoke();
+
+        public async Task GetBananas()
+        {
+            Bananas = await _http.GetFromJsonAsync<int>("api/User/GetBananas");
+            BananasChanged();
+        }
     }
 }
