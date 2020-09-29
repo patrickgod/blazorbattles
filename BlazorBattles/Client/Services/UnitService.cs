1893c29a5b0ca8973461b3417e22211909f3d79b
Patrick <patrick@godknows.de>
2020-09-29 19:07:42 +0200

    Build UserUnits on the Client

--- BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -13,22 +13,32 @@ namespace BlazorBattles.Client.Services
     {
         private readonly IToastService _toastService;
         private readonly HttpClient _http;
+        private readonly IBananaService _bananaService;
 
-        public UnitService(IToastService toastService, HttpClient http)
+        public UnitService(IToastService toastService, HttpClient http, IBananaService bananaService)
         {
             _toastService = toastService;
             _http = http;
+            _bananaService = bananaService;
         }
 
         public IList<Unit> Units { get; set; } = new List<Unit>();
 
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
 
-        public void AddUnit(int unitId)
+        public async Task AddUnit(int unitId)
         {
             Unit unit = Units.First(unit => unit.Id == unitId);
-            MyUnits.Add(new UserUnit { UnitId = unit.Id, HitPoints = unit.HitPoints });
-            _toastService.ShowSuccess($"Your {unit.Title} has been built!", "Unit built!");
+            var result = await _http.PostAsJsonAsync<int>("api/UserUnit", unitId);
+            if (result.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                _toastService.ShowError(await result.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                await _bananaService.GetBananas();
+                _toastService.ShowSuccess($"Your {unit.Title} has been built!", "Unit built!");
+            }
         }
 
         public async Task LoadUnitsAsync()
