e47b3dad4fcbe705fe5b71167ed5d369d97b1d1c
Patrick <patrick@godknows.de>
2020-09-29 05:54:25 +0200

    Add the Authentication Controller

---
+++ BlazorBattles/Server/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorBattles.Server.Data;
+using BlazorBattles.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorBattles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegister request)
+        {
+            var response = await _authRepo.Register(
+                    new User
+                    {
+                        Username = request.Username,
+                        Email = request.Email,
+                        Bananas = request.Bananas,
+                        DateOfBirth = request.DateOfBirth,
+                        IsConfirmed = request.IsConfirmed
+                    }, request.Password
+                );
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
