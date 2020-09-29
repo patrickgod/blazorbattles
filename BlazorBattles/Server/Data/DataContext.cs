a39af36b6b42e6dca8d7e8a17747b12276b24b0c
Patrick <patrick@godknows.de>
2020-09-29 18:21:54 +0200

    A One-to-Many Relation: UserUnits

--- BlazorBattles/Server/Data/DataContext.cs
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -14,5 +14,7 @@ namespace BlazorBattles.Server.Data
         public DbSet<Unit> Units { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<UserUnit> UserUnits { get; set; }
     }
 }
--- BlazorBattles/Shared/User.cs
+++ BlazorBattles/Shared/User.cs
@@ -16,5 +16,6 @@ namespace BlazorBattles.Shared
         public bool IsConfirmed { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
+        public List<UserUnit> Units { get; set; }
     }
 }
