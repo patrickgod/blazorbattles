d87bc9b48fba503d441a75be90a41f8242044f1a
Patrick <patrick@godknows.de>
2020-09-29 05:04:19 +0200

    The User Model

--- BlazorBattles/Server/Data/DataContext.cs
+++ BlazorBattles/Server/Data/DataContext.cs
@@ -9,11 +9,10 @@ namespace BlazorBattles.Server.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Unit> Units { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
