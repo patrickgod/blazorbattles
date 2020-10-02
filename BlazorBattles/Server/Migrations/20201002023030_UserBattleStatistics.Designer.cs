9879ad194d5391dd07439380d0761c5fc4169e33
Patrick <patrick@godknows.de>
2020-10-02 04:33:03 +0200

    Add Battle Properties to User Model

--- BlazorBattles/Server/Migrations/20201002023030_UserBattleStatistics.Designer.cs
+++ BlazorBattles/Server/Migrations/20201002023030_UserBattleStatistics.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BlazorBattles.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlazorBattles.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201002023030_UserBattleStatistics")]
+    partial class UserBattleStatistics
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
