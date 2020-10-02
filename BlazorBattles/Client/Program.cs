0c8b1950bd604272a78c7f704b01cdd44ff1c5b9
Patrick <patrick@godknows.de>
2020-10-02 04:51:35 +0200

    Create the Leaderboard Service

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -30,6 +30,7 @@ namespace BlazorBattles.Client
             builder.Services.AddOptions();
             builder.Services.AddAuthorizationCore();
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+            builder.Services.AddScoped<ILeaderboardService, LeaderboardService>();
 
             await builder.Build().RunAsync();
         }
