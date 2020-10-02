98103bd9e41f0ecbdda1159d2011ea3218195693
Patrick <patrick@godknows.de>
2020-10-02 04:45:54 +0200

    Implement the Leaderboard

--- BlazorBattles/Server/Controllers/UserController.cs
+++ BlazorBattles/Server/Controllers/UserController.cs
@@ -43,5 +43,31 @@ namespace BlazorBattles.Server.Controllers
             await _context.SaveChangesAsync();
             return Ok(user.Bananas);
         }
+
+        [HttpGet("Leaderboard")]
+        public async Task<IActionResult> GetLeaderboard()
+        {
+            var users = await _context.Users.Where(user => !user.IsDeleted).ToListAsync();
+
+            users = users
+                .OrderByDescending(u => u.Victories)
+                .ThenBy(u => u.Defeats)
+                .ThenBy(u => u.DateCreated)
+                .ToList();
+
+            int rank = 1;
+            var response = users.Select(
+                user => new UserStatistic
+                {
+                    Rank = rank++,
+                    UserId = user.Id,
+                    Username = user.Username,
+                    Battles = user.Battles,
+                    Victories = user.Victories,
+                    Defeats = user.Defeats
+                });
+
+            return Ok(response);
+        }
     }
 }
